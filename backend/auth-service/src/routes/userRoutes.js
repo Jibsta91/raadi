338bc09d0238ac89ec4ae992426d704f0904d5d7
Najib Adbi <najib.abdi@sporveien.com>
2024-05-22 18:23:27 +0200

    rewrite auth-listingservice

--- backend/auth-service/src/routes/userRoutes.js
+++ backend/auth-service/src/routes/userRoutes.js
@@ -1,34 +1,8 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const express = require('express');
+const { registerUser, authUser } = require('../controllers/userController');
+const router = express.Router();
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-});
+router.post('/register', registerUser);
+router.post('/login', authUser);
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+module.exports = router;
